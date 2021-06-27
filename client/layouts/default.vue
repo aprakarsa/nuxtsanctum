33bff1fee3c8c028c702d7cf2abec78b8c0e985c
Aditya Prakarsa <aprakarsa@gmail.com>
2021-06-27 17:02:43 +0800

    add api and client app

---
+++ client/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-if="$auth.loggedIn">
+      {{ $auth.user.name }}
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ client/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <a href="#" @click.prevent="login">Login</a>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth.loginWith("laravelSanctum", {
+        data: {
+          email: "aprakarsa@gmail.com",
+          password: "password"
+        }
+      });
+    }
+  }
+};
+</script>
